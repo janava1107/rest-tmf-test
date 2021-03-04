bc68fea11a420cc1488f837192532672ea21d873
Javier Navarrete Vera <jnavaver@everis.com>
2021-03-03 22:03:36 -0300

    get con sn

--- controllers/welcome.controllers.js
+++ controllers/welcome.controllers.js
@@ -5,15 +5,33 @@ const welcomeGet = (req, res) => {
     
     console.log('welcomeget')
     
-    const {id,canal} = req.query;
-    res.json({
-        msg: 'endpoint get',
-        id,
-        canal
+    const {sn,canal} = req.query;
+    request.get('https://2c7b355e-3990-42fb-994b-e34ad6bb1887.mock.pstmn.io/product/?serviceNumber='+sn, function (error, resp, body) {
+        
+        if (!error && resp.statusCode === 200) {
+
+            resp_tmf = JSON.parse(body);
+            console.log('rating: ',resp_tmf.ratingType);
+            console.log('estado: ',resp_tmf.status);
+            console.log('tenant: ',resp_tmf.tenant.id);
+            
+            res.json({
+                //msg: 'endpoint post',
+                sn,
+                canal,
+                rating:resp_tmf.ratingType,
+                status:resp_tmf.status,
+                tenant:resp_tmf.tenant.id
+            });
+
+        }else{
+            res.json(error);
+        }    
+        
     });
 }
 
-const welcomePost = (req, res = response) => {
+const welcomePost = (req, res) => {
     console.log('welcomepost');
     const {sn,canal} = req.body;
     
