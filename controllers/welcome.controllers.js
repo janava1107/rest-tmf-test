4034da6d1d2fb2a50438a575bb96951e2e617b49
Javier Navarrete Vera <jnavaver@everis.com>
2021-03-05 11:25:45 -0300

    limpiando bff

--- controllers/welcome.controllers.js
+++ controllers/welcome.controllers.js
@@ -5,7 +5,8 @@ const welcomeGet = (req, res) => {
     
     console.log('welcomeget')
     
-    const {sn,canal} = req.query;
+    //const {sn,canal} = req.query;
+    const {sn} = req.query;
     request.get('https://2c7b355e-3990-42fb-994b-e34ad6bb1887.mock.pstmn.io/product/?serviceNumber='+sn, function (error, resp, body) {
         
         if (!error && resp.statusCode === 200) {
@@ -18,7 +19,7 @@ const welcomeGet = (req, res) => {
             res.json({
                 //msg: 'endpoint post',
                 sn,
-                canal,
+                //canal,
                 rating:resp_tmf.ratingType,
                 status:resp_tmf.status,
                 tenant:resp_tmf.tenant.id
