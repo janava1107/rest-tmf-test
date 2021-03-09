ee4880040383f0aaf07eac1d98629d4a5369ffa7
Javier Navarrete Vera <jnavaver@everis.com>
2021-03-09 19:50:41 -0300

    aa

--- controllers/welcome.controllers.js
+++ controllers/welcome.controllers.js
@@ -28,43 +28,20 @@ const welcomeGet = (req, res) => {
 
 const welcomePost = (req, res) => {
     console.log('welcomepost');
-    const {sn,tipo} = req.body;
+    const {sn,canal} = req.body;
     
     axios.get('https://2c7b355e-3990-42fb-994b-e34ad6bb1887.mock.pstmn.io/product/?serviceNumber='+sn).then(resp=> {
         
-        //cambiar por case comentado y ajustar cloud function
-        switch(tipo){
-        //switch(resp.data.ratingType){
-            case 'Hybrid':
-                var ratingType='Hybrid';
-                var status=resp.data.status;
-                var baseType=resp.data.tenant['@baseType'];
-            break;
-            case 'pre':
-                var respuesta={
-                    ratingType:'Hybrid',
-                    status:resp.data.status,
-                    baseType:resp.data.tenant['@baseType']
-                }
-            break;
-            case 'post':
-                var ratingType='Postpay';
-                var status=resp.data.status;
-                var baseType=resp.data.tenant['@baseType'];
-            break;        
-        }
-        
         //resp original
-        res.json(
-            respuesta
-            //sn,
-            //rating:ratingType,
-            // status:status,
-            // tenant_baseType:baseType,
-            // tenant_schemaLocation:resp.data.tenant['@schemaLocation'],
-            // tenant_type:resp.data.tenant['@type'],
-            // tenant:resp.data.tenant.id
-        );
+        res.json({
+            sn,
+            rating:resp.data.ratingType,
+            status:resp.data.status,
+            tenant_baseType:resp.data.tenant['@baseType'],
+            tenant_schemaLocation:resp.data.tenant['@schemaLocation'],
+            tenant_type:resp.data.tenant['@type'],
+            tenant:resp.data.tenant.id
+        });
         
     })
     .catch((error) => {
