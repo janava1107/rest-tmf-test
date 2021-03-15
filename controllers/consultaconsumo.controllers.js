23f8336fadf4357df1dfa87c4a6566b96ed725ad
Javier Navarrete Vera <jnavaver@everis.com>
2021-03-15 19:52:02 -0300

    consulta consumo casos

--- controllers/consultaconsumo.controllers.js
+++ controllers/consultaconsumo.controllers.js
@@ -4,6 +4,9 @@ const consultaconsumoGet = (req, res) => {
     console.log('consultaconsumoget')
     
     const {sn,ert} = req.query;
+    
+    
+    /*
     axios.get('https://2c7b355e-3990-42fb-994b-e34ad6bb1887.mock.pstmn.io/usageConsumptionReport/?serviceNumber='+sn).then(resp=> {
         res.json({
             sn,
@@ -32,13 +35,219 @@ const consultaconsumoGet = (req, res) => {
     })
     .finally(() => {
         console.log('finalizado!!');
-    })
+    })*/
 }
 
 const consultaconsumoPost = (req, res) => {
     console.log('consultaconsumopost');
     const {sn,ert} = req.body;
-    axios.get('https://2c7b355e-3990-42fb-994b-e34ad6bb1887.mock.pstmn.io/usageConsumptionReport/?serviceNumber='+sn).then(resp=> {
+
+    switch(sn){
+        //data
+        case '56999999999':
+            res.json({
+                "sn": "56999999999",
+                "tenant_id": "WOM-Chile",
+            
+                "bucket_usageType_data": "Data",
+                "bucket_initialBalance_data": "10GB",
+                "bucketBalance_remainingBalanceAmount_data": "8GB",
+                "bucket_name_data": "10Gigas of Data",
+                "bucket_isUnlimited_data": false,
+            
+                "bucket_usageType_voice": "",
+                "bucket_initialBalance_voice": "",
+                "bucketBalance_remainingBalanceAmount_voice": "",
+                "bucket_name_voice": "",
+                "bucket_isUnlimited_voice": false,
+            
+                "bucket_usageType_currency": "",
+                "bucket_initialBalance_currency": "",
+                "bucketBalance_remainingBalanceAmount_currency": "",
+                "bucket_name_currency": "",
+                "bucket_isUnlimited_currency": false,
+            
+                "bucket_usageType_sms": "",
+                "bucket_initialBalance_sms": "",
+                "bucketBalance_remainingBalanceAmount_sms": "",
+                "bucket_name_sms": "",
+                "bucket_isUnlimited_sms": false,
+            
+                "tenant_schemaLocation": "urn:wom:UsageConsumption:/schema/2.0.0"
+            })
+            break;
+
+        //data-voice    
+
+        case '56888888888':
+            res.json({
+                "sn": "56888888888",
+                "tenant_id": "WOM-Chile",
+
+                "bucket_usageType_data": "Data",
+                "bucket_initialBalance_data": "10GB",
+                "bucketBalance_remainingBalanceAmount_data": "8GB",
+                "bucket_name_data": "10Gigas of Data",
+                "bucket_isUnlimited_data": false,
+
+                "bucket_usageType_voice": "Voice",
+                "bucket_initialBalance_voice": "100Min",
+                "bucketBalance_remainingBalanceAmount_voice": "88Min",
+                "bucket_name_voice": "10Gigas of Data and 100Min of Voice",
+                "bucket_isUnlimited_voice": false,
+
+                "bucket_usageType_currency": "",
+                "bucket_initialBalance_currency": "",
+                "bucketBalance_remainingBalanceAmount_currency": "",
+                "bucket_name_currency": "",
+                "bucket_isUnlimited_currency": false,
+
+                "bucket_usageType_sms": "",
+                "bucket_initialBalance_sms": "",
+                "bucketBalance_remainingBalanceAmount_sms": "",
+                "bucket_name_sms": "",
+                "bucket_isUnlimited_sms": false,
+
+                "tenant_schemaLocation": "urn:wom:UsageConsumption:/schema/2.0.0"
+            })
+            break; 
+
+        //Data-voice-currency
+        case '56777777777':
+            res.json({
+                "sn": "56777777777",
+                "tenant_id": "WOM-Chile",
+
+                "bucket_usageType_data": "Data",
+                "bucket_initialBalance_data": "10GB",
+                "bucketBalance_remainingBalanceAmount_data": "8GB",
+                "bucket_name_data": "10Gigas of Data",
+                "bucket_isUnlimited_data": false,
+
+                "bucket_usageType_voice": "Voice",
+                "bucket_initialBalance_voice": "100Min",
+                "bucketBalance_remainingBalanceAmount_voice": "88Min",
+                "bucket_name_voice": "10Gigas of Data and 100Min of Voice",
+                "bucket_isUnlimited_voice": false,
+
+                "bucket_usageType_currency": "Currency",
+                "bucket_initialBalance_currency": "",
+                "bucketBalance_remainingBalanceAmount_currency": "",
+                "bucket_name_currency": "",
+                "bucket_isUnlimited_currency": false,
+
+                "bucket_usageType_sms": "",
+                "bucket_initialBalance_sms": "",
+                "bucketBalance_remainingBalanceAmount_sms": "",
+                "bucket_name_sms": "",
+                "bucket_isUnlimited_sms": false,
+
+                "tenant_schemaLocation": "urn:wom:UsageConsumption:/schema/2.0.0"
+            })
+            break;
+        
+        //Data-voice-currency-sms    
+        case '56666666666':
+            res.json({
+                "sn": "56666666666",
+                "tenant_id": "WOM-Chile",
+
+                "bucket_usageType_data": "Data",
+                "bucket_initialBalance_data": "10GB",
+                "bucketBalance_remainingBalanceAmount_data": "8GB",
+                "bucket_name_data": "10Gigas of Data",
+                "bucket_isUnlimited_data": false,
+
+                "bucket_usageType_voice": "Voice",
+                "bucket_initialBalance_voice": "100Min",
+                "bucketBalance_remainingBalanceAmount_voice": "88Min",
+                "bucket_name_voice": "10Gigas of Data and 100Min of Voice",
+                "bucket_isUnlimited_voice": false,
+
+                "bucket_usageType_currency": "Currency",
+                "bucket_initialBalance_currency": "",
+                "bucketBalance_remainingBalanceAmount_currency": "",
+                "bucket_name_currency": "Data currency",
+                "bucket_isUnlimited_currency": false,
+
+                "bucket_usageType_sms": "SMS",
+                "bucket_initialBalance_sms": "100",
+                "bucketBalance_remainingBalanceAmount_sms": "98",
+                "bucket_name_sms": "Data sms",
+                "bucket_isUnlimited_sms": false,
+
+                "tenant_schemaLocation": "urn:wom:UsageConsumption:/schema/2.0.0"
+            })
+            break;
+            
+        case '56555555555':
+            res.json({
+                "sn": "56555555555",
+                "tenant_id": "WOM-Chile",
+
+                "bucket_usageType_data": "Data",
+                "bucket_initialBalance_data": "10GB",
+                "bucketBalance_remainingBalanceAmount_data": "8GB",
+                "bucket_name_data": "10Gigas of Data",
+                "bucket_isUnlimited_data": true,
+
+                "bucket_usageType_voice": "Voice",
+                "bucket_initialBalance_voice": "100Min",
+                "bucketBalance_remainingBalanceAmount_voice": "88Min",
+                "bucket_name_voice": "10Gigas of Data and 100Min of Voice",
+                "bucket_isUnlimited_voice": true,
+
+                "bucket_usageType_currency": "",
+                "bucket_initialBalance_currency": "",
+                "bucketBalance_remainingBalanceAmount_currency": "",
+                "bucket_name_currency": "",
+                "bucket_isUnlimited_currency": false,
+
+                "bucket_usageType_sms": "",
+                "bucket_initialBalance_sms": "",
+                "bucketBalance_remainingBalanceAmount_sms": "",
+                "bucket_name_sms": "",
+                "bucket_isUnlimited_sms": false,
+
+                "tenant_schemaLocation": "urn:wom:UsageConsumption:/schema/2.0.0"
+            })
+            break; 
+
+        default:
+            res.json({
+                "sn": "56555555555",
+                "tenant_id": "WOM-Chile",
+
+                "bucket_usageType_data": "Data",
+                "bucket_initialBalance_data": "10GB",
+                "bucketBalance_remainingBalanceAmount_data": "8GB",
+                "bucket_name_data": "10Gigas of Data",
+                "bucket_isUnlimited_data": true,
+
+                "bucket_usageType_voice": "Voice",
+                "bucket_initialBalance_voice": "100Min",
+                "bucketBalance_remainingBalanceAmount_voice": "88Min",
+                "bucket_name_voice": "10Gigas of Data and 100Min of Voice",
+                "bucket_isUnlimited_voice": true,
+
+                "bucket_usageType_currency": "",
+                "bucket_initialBalance_currency": "",
+                "bucketBalance_remainingBalanceAmount_currency": "",
+                "bucket_name_currency": "",
+                "bucket_isUnlimited_currency": false,
+
+                "bucket_usageType_sms": "",
+                "bucket_initialBalance_sms": "",
+                "bucketBalance_remainingBalanceAmount_sms": "",
+                "bucket_name_sms": "",
+                "bucket_isUnlimited_sms": false,
+
+                "tenant_schemaLocation": "urn:wom:UsageConsumption:/schema/2.0.0"
+            })
+        }        
+
+
+    /*axios.get('https://2c7b355e-3990-42fb-994b-e34ad6bb1887.mock.pstmn.io/usageConsumptionReport/?serviceNumber='+sn).then(resp=> {
         //console.log(resp.data);
         res.json({
             sn,
@@ -66,7 +275,7 @@ const consultaconsumoPost = (req, res) => {
     })
     .finally(() => {
         console.log('finalizado!!');
-    })
+    }) */
     
     
 }
